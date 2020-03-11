7bad2992aaabc7f083b7cdd22fe8ee986ffe0532
Mike Desaro <mike@prime31.com>
2020-03-11 16:14:54 -0700

    prep

--- completer.py
+++ completer.py
@@ -8,10 +8,18 @@ def fart():
 	print('----------- fart')
 
 
-# completions come in a few different flavors:
+# completions come in a few different flavors based on the context of the cursor:
 #	- naked: there is no previous '.' in the completion string. Completions here could be:
-#		- local procs in the current file
-#		- module names (or aliases if used)
+#		- procs in the current package
+#		- types in the package
+#		- module names (or aliases if used) for any imports
+#		- local variable names
+#	- dotted module: there is 'module.' and the word before the '.' is a module name or alias
+#		- procs in the module
+#		- types in the module
+#		- exposed globals in the module
+#	- dotted non-module:
+#		- most likely a local variable. Too hard to figure out its type so forget it.
 class Completer(object):
 	def fart(self):
 		print('------- wha asflkjdf')
