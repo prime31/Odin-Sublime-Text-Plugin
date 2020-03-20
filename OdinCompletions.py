bc1ea1cc92ec868739540d0c8aec118e792d2a9e
Mike Desaro <mike@prime31.com>
2020-03-19 17:17:57 -0700

    cleaning

--- OdinCompletions.py
+++ OdinCompletions.py
@@ -5,28 +5,16 @@ import os
 import glob
 import fnmatch
 import time
-from Odin import completer
 from Odin import parser
 
 
 class OdinCompletions(sublime_plugin.EventListener):
-  completer = completer.Completer()
-  # when filtering for user dirs exclude these. Note that 'libs' is excluded only because 'projects' is in 'shared' temporarily
-  exclude_dirs = set(['.git', 'libs'])
   alias_to_package = dict()
 
-  import_pattern = re.compile(r'(import\s.*\n)')
-  package_pattern = re.compile(r'package\s(.*)')
+  package_pattern = re.compile(r'package\s+(.*)')
   core_package_pattern = re.compile(r'import\s(\w+|)\s*\"(?:core:)+(.*?)\"')
   shared_package_pattern = re.compile(r'import\s(\w+|)\s*\"(?:shared:)+(.*?)\"')
   local_package_pattern = re.compile(r'import\s(\w+|)\s*\"(?!.*?:)(\w+)+(.*?)\"')
-
-  definition_pattern = re.compile(r'\b\w+\s*:[\w\W]*?(?:[{;]|[-]{3})')
-  struct_pattern = re.compile(r'(\b\w+)\s*::\s*struct\s*[\w\W]*?[{;]')
-  line_comment_pattern = re.compile(r'//.*?(?=\n)')
-  proc_pattern = re.compile(r'\b(\w+)\s*:\s*[:=]\s*proc\(([\w\W]*?)\)\s*(?:->\s*(.*?)\s)?')
-  proc_differentiator_pattern = re.compile(r'proc\(.*?\)')
-  proc_params_pattern = re.compile(r'(?:^|)\s*([^,]+?)\s*(?:$|,)')
 
   # the package of the current file
   current_package = ''
@@ -47,22 +35,6 @@ class OdinCompletions(sublime_plugin.EventListener):
       if v == package:
         return k
     return package
-
-  def view_is_odin(self, view):
-    settings = view.settings()
-
-    if settings != None:
-      syntax = settings.get('syntax')
-      if syntax != None:
-        if syntax.find('odin.sublime-syntax') >= 0:
-          return True
-
-    path = view.file_name()
-
-    if path != None and path[-5:].lower() == '.odin':
-      return True
-
-    return False
 
   def get_all_odin_file_paths(self):
     paths = set()
@@ -86,6 +58,7 @@ class OdinCompletions(sublime_plugin.EventListener):
 
 
     if is_local_package_completion and not is_var_field_access:
+      self.search_package = word_before_dot
       if len(self.included_local_packages) > 0:
         curr_path = os.path.dirname(sublime.active_window().active_view().file_name())
         for root, dirs, files in os.walk(curr_path):
@@ -96,6 +69,7 @@ class OdinCompletions(sublime_plugin.EventListener):
               paths.add(os.path.join(root, file))
 
     if is_shared_package_completion and not is_var_field_access:
+      self.search_package = word_before_dot
       # include any imported shared packages
       if len(self.included_shared_packages) > 0:
         odin_shared_path = os.path.expanduser('~/odin/shared')
@@ -107,6 +81,7 @@ class OdinCompletions(sublime_plugin.EventListener):
               paths.add(os.path.join(root, file))
 
     if is_core_package_completion and not is_var_field_access:
+      self.search_package = word_before_dot
       # include any imported core packages
       if len(self.included_core_packages) > 0:
         odin_lib_path = os.path.expanduser('~/odin/core')
@@ -124,127 +99,11 @@ class OdinCompletions(sublime_plugin.EventListener):
 
   def get_file_contents(self, file_path):
     file_view = sublime.active_window().find_open_file(file_path)
-
     if file_view == None:
       with open(file_path, 'r', encoding='utf-8') as f:
         return f.read()
     else:
       return file_view.substr(sublime.Region(0, file_view.size()))
-
-  def strip_block_comments(self, odin_text):
-    non_comments = []
-
-    comment_end_index = 0
-    block_comment_depth = 0
-
-    for i in range(len(odin_text)):
-      if odin_text[i:i+2] == '/*':
-        if block_comment_depth == 0:
-          non_comment = odin_text[comment_end_index:i]
-          non_comments.append(non_comment)
-        block_comment_depth += 1
-      elif odin_text[i-2:i] == '*/':
-        block_comment_depth -= 1
-        if block_comment_depth == 0:
-          comment_end_index = i
-        if block_comment_depth < 0:
-          block_comment_depth = 0
-
-    non_comments.append(odin_text[comment_end_index:])
-    return ''.join(non_comments)
-
-  def strip_line_comments(self, odin_text):
-    return self.line_comment_pattern.sub('', odin_text)
-
-  def strip_imports(self, odin_text):
-    return self.import_pattern.sub('', odin_text)
-
-  def make_completion_from_proc_components(self, proc_name, params, return_type, file_name):
-    trigger = proc_name + '('
-    result = proc_name + '('
-
-    if len(params) > 0:
-      for p in range(len(params)):
-        if p > 0:
-          trigger += ', '
-          result += ', '
-
-        trigger += params[p]
-        result += '${' + str(p+1) + ':' + params[p] + '}'
-
-    trigger += ')'
-    result += ')'
-
-    if return_type != None:
-      trigger += ' -> ' + return_type
-
-    trigger += '\t ' + file_name
-
-    # proc completion
-    completion = [trigger, result]
-
-    # TODO: not sure these are required. they make the results too noisy
-    # param completions
-    # for param in params:
-    #   completion = self.make_completion_from_variable_definition(param, file_name)
-    #   completions.append(completion)
-
-    return completion
-
-  def extract_definitions_from_text(self, odin_text):
-    defs = self.definition_pattern.findall(odin_text)
-    return list(set(defs))
-
-  def make_completion_from_proc_definition(self, definition, file_name):
-    match = self.proc_pattern.match(definition)
-    if match == None:
-      return []
-    groups = match.groups()
-
-    proc_name = groups[0]
-    params_str = groups[1]
-    return_type = groups[2]
-
-    params_list = self.proc_params_pattern.findall(params_str)
-
-    # Handle the case of empty space inside parentheses
-    if len(params_list) == 1 and params_list[0].strip() == '':
-      params_list = []
-
-    return self.make_completion_from_proc_components(proc_name, params_list, return_type, file_name)
-
-  def make_completion_from_variable_definition(self, definition, file_name):
-    colon_index = definition.find(':')
-    identifier = definition[:colon_index].strip()
-    return [identifier + '\t ' + file_name, identifier]
-
-  def get_completions_from_file_path(self, path):
-    contents = self.get_file_contents(path)
-    contents = self.strip_imports(contents)
-    # Currently unused in order to maximize speed
-    # contents = self.strip_block_comments(contents)
-    # contents = self.strip_line_comments(contents)
-
-    definitions = self.extract_definitions_from_text(contents)
-
-    completions = []
-    file_name = os.path.split(path)[1]
-
-    for definition in definitions:
-      if self.proc_differentiator_pattern.search(definition) == None:
-        struct_match = self.struct_pattern.match(definition)
-        if struct_match != None:
-          struct_name = struct_match.groups()[0]
-          completions.append([struct_name + '\t struct', struct_name])
-
-        # TODO: this is a bit too loosey-goosey including every variable declaration
-        #completion = self.make_completion_from_variable_definition(definition, file_name)
-        #print('[[' + definition + ']] ---- ' + completion[0])
-        #completions.append(completion)
-      else:
-        completions.append(self.make_completion_from_proc_definition(definition, file_name))
-
-    return completions
 
   # return True (if the next char is empty or a newline) and all the previous chars are valid proc/var name chars up to the '.'
   def is_dot_completion(self, file_view, loc):
@@ -292,13 +151,8 @@ class OdinCompletions(sublime_plugin.EventListener):
       self.alias_to_package[alias] = package
       self.included_local_packages.append(package)
 
-  def on_activated_async(self, view):
-    pass
-    # this only works if the code is able to be compiled and we are in a file with main()
-    #self.completer.index_file(view.file_name())
-
   def on_query_completions(self, view, prefix, locations):
-    if len(locations) > 1:
+    if len(locations) > 1 or not view.file_name().endswith('.odin'):
       return None
 
     # cleanup before we start
@@ -308,9 +162,6 @@ class OdinCompletions(sublime_plugin.EventListener):
     self.included_local_packages.clear()
     self.before_dot = None
     self.search_package = None
-
-    if not self.view_is_odin(view):
-      return None
 
     # dont bother with completions if we are in a comment block or string
     scope_name = view.scope_name(locations[0])
@@ -367,8 +218,6 @@ class OdinCompletions(sublime_plugin.EventListener):
     for path in reversed(list(paths)):
       package_or_filename = self.search_package or os.path.split(path)[1]
       completions += parser.get_completions_from_file(package_or_filename, self.get_file_contents(path))
-      # TODO: remove old completions
-      # completions += self.get_completions_from_file_path(path)
 
     # Report time spent building completions before returning
     delta_time_ms = int((time.time() - start_time) * 1000)
