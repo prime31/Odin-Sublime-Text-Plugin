06c2593fc6e448b9b8e304738da0efd38b53151a
Mike <mike@prime31.com>
2020-03-27 23:27:13 -0700

    stuff

--- General.py
+++ General.py
@@ -3,12 +3,16 @@ import sublime_plugin
 
 class CloseMinimapOnMultiView(sublime_plugin.EventListener):
     was_minimap_open = True
+    was_sidebar_visible = True
 
     def on_post_window_command(self, window, cmd_name, _args):
         if cmd_name in ['set_layout', 'new_pane', 'close_pane']:
             if len(window.get_layout()['cols']) > 2:
                 # save the state of the minimap so that we can conditionally restore it when back to 1 column
                 self.was_minimap_open = window.is_minimap_visible()
+                self.was_sidebar_visible = window.is_sidebar_visible()
                 window.set_minimap_visible(False)
+                window.set_sidebar_visible(False)
             else:
                 window.set_minimap_visible(self.was_minimap_open)
+                window.set_sidebar_visible(self.was_sidebar_visible)
