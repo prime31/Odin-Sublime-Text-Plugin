8aae6c02b0c7ab6ea578eb468cd2d7d2b89c5a48
Mike Desaro <mike@prime31.com>
2020-04-08 11:10:10 -0700

    snake caser

--- ContextCommands.py
+++ ContextCommands.py
@@ -6,6 +6,12 @@ import threading
 import re
 
 
+to_snake_case_pattern = re.compile(r'(?!^)(?<!_)([A-Z])')
+
+def to_snake_case(txt):
+	return to_snake_case_pattern.sub(r'_\1', txt).lower()
+
+# todo: double check this and convert AdaCaseSelectionCommand to use it like SnakeCaseSelectionCommand uses to_snake_case
 def to_ada_case(txt):
 	new_txt = ''
 
@@ -63,6 +69,28 @@ class AdaCaseSelectionCommand(sublime_plugin.TextCommand):
 				else:
 					new_txt += txt[i]
 			self.view.replace(edit, region, new_txt)
+
+
+class SnakeCaseSelectionCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sel = self.view.sel()
+		regions_by_line = self.view.split_by_newlines(sel[0])
+		word_match_pattern = re.compile(r'^\s*(\w+)') # extracts the first word
+		replacements = {}
+
+		for region in regions_by_line:
+			txt = self.view.substr(region)
+			matches = word_match_pattern.findall(txt)
+			if len(matches) == 0:
+				continue
+
+			txt = txt.replace(matches[0], to_snake_case(matches[0]))
+			replacements[matches[0]] = to_snake_case(matches[0])
+
+		full_region_txt = self.view.substr(sel[0])
+		for orig_word in replacements:
+			full_region_txt = full_region_txt.replace(orig_word, replacements[orig_word])
+		self.view.replace(edit, self.view.full_line(sel[0]), full_region_txt)
 
 
 class ToUpperSelectionCommand(sublime_plugin.TextCommand):
